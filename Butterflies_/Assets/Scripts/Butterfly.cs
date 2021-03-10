6e3389de8ad988aa75baac3ce8448c78a1ea7750
21phips <79919172+21phips@users.noreply.github.com>
2021-03-10 09:15:33 -0500

    Collision and Scene Management

--- Butterflies_/Assets/Scripts/Butterfly.cs
+++ Butterflies_/Assets/Scripts/Butterfly.cs
@@ -1,28 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Butterfly : MonoBehaviour
 {
     public float flySpeed = 3f;
     private Rigidbody rb; 
     public float rotateSpeed = 2.0f;
+    bool isAlive = true;
+    int currentLevel;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //flying movement
-        Fly();
+        if (isAlive)
+        {
+            //flying movement
+            Fly();
 
-        //roatate movement
-        Rotate();
+            //roatate movement
+            Rotate();
+        }
+        
+    
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bad")
+        {
+            //Die
+            isAlive = false;
+            //Reset
+            SceneManager.LoadScene(currentLevel);
+        
+        }
+    }
+
+
     void Fly()
     {
         if (Input.GetKey(KeyCode.Space))
